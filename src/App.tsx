8e352a509eabcbeb8e9f2a5b4e7a50039b52625c
Hamad Javed <hamadjaved69@gmail.com>
2020-04-17 17:21:12 +0500

    Completed the logic to take values from input field and display them using state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My Skills</h1>
-        <p>Coming Soon</p>
-      </header>
-    </div>
-  );
+class App extends React.Component<any, any> {
+  
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      skills: [],
+      userInput: ''
+    }
+    //binding the onChange
+    this.onGetValues = this.onGetValues.bind(this);
+  }
+
+  onGetValues(e: { target: { value: any; }; }) {
+    const inputValue = e.target.value;
+    const itemsArray = inputValue.split(',');
+    this.setState({
+      skills: itemsArray,
+      userInput: e.target.value
+    })
+  }
+  render() {
+    const item = this.state.skills.map((element: any, index: number)=> {
+      return <li key={index}>{element}</li>;
+    });
+    return (
+      <div className="App text-center element">
+          <h1>Enter Your Skills</h1>
+          <input 
+            type="text"
+            onChange={this.onGetValues}
+            value={this.state.userInput}
+          />
+          <br />
+          <code>Enter Comma Separated Values e.g., html, css, angular</code>
+          <div className="container">
+            {this.state.userInput.length > 0 ? <ul>{item}</ul> : <code>Start Writing</code>}
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
